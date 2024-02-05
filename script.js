100f7f566e468dd32b1d2a32f0eb0378dd4778e1
Andy-Almeida <andyalmeida191@gmail.com>
2024-02-04 18:43:13 -0800

    Modify script so scroll to the top of the page for home

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// JavaScript to offset scroll position by the height of the fixed header
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    // JavaScript to offset scroll position by the height of the fixed header
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute('href'));
-        const headerHeight = document.querySelector('header').offsetHeight; // Get the height of the fixed header
+            const target = document.querySelector(this.getAttribute('href'));
+            const headerHeight = document.querySelector('header').offsetHeight; // Get the height of the fixed header
 
-        window.scrollTo({
-            top: target.offsetTop - headerHeight + 10,
-            behavior: 'smooth'
+            if (this.getAttribute('href') === '#home') {
+                // If clicking the #home link, scroll to the top of the page
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            } else {
+                // Otherwise, scroll to the target section with offset for fixed header
+                window.scrollTo({
+                    top: target.offsetTop - headerHeight,
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 });
