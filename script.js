f6a48e1f7553edd5a35ed1eac462ca395430ec4f
pie240 <andyalmeida191@gmail.com>
2024-02-04 19:54:45 -0800

    Working on dropdown

--- script.js
+++ script.js
@@ -25,3 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('dropdown_menu')
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open')
+}
+
