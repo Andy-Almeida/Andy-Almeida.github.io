8be0fa39889f3df064d8b6009ac3844c40ebc497
Andy-Almeida <andyalmeida191@gmail.com>
2024-02-04 18:32:42 -0800

    Add a Javascript file, added a function to offset scroll position by the height of the fixd header

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript to offset scroll position by the height of the fixed header
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const headerHeight = document.querySelector('header').offsetHeight; // Get the height of the fixed header
+
+        window.scrollTo({
+            top: target.offsetTop - headerHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
