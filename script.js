0e1b9d8785dac3e5de44fcebb474cdeaa3afe65b
pie240 <andyalmeida191@gmail.com>
2024-02-04 22:22:30 -0800

    Fixed the JS for the drop down menu

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('dropdown_menu')
+const dropDownMenu = document.querySelector('.dropdown_menu')
 
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle('open')
